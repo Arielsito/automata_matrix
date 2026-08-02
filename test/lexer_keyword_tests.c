2bcc86fc5560ce0c5c44ba3c6b828b9118349cb8
Arielsito <ang14012006@gmail.com>
2026-08-02 16:30:47 -0600

    update lexer keyword tests and add all tests to CMakeLists file

--- test/lexer_keyword_tests.c
+++ test/lexer_keyword_tests.c
@@ -11,7 +11,7 @@ static Token next_token(const char *src) {
   init_scan(src);
   return scan_token();
 }
-// keywords
+
 typedef struct {
   char input[16];
   TokenType expected;
@@ -56,9 +56,60 @@ ParameterizedTestParameters(lexer_kws, keywords) {
   size_t count = sizeof(cases) / sizeof(KeywordCase);
   return cr_make_param_array(KeywordCase, cases, count);
 }
+
 ParameterizedTest(KeywordCase *tc, lexer_kws, keywords) {
   Token t = next_token(tc->input);
   cr_assert_eq(t.type, tc->expected,
       "Error in keyword '%s': expected '%d' but got '%d'", tc->input, tc->expected, t.type
       );
+  cr_assert_eq(t.length, (i32)strlen(tc->input),
+      "'%s' completed (len %d, got %d)", tc->input, (i32)strlen(tc->input), t.length
+      );
+  Token e = scan_token();
+  cr_assert_eq(e.type, TOKEN_EOF,
+      "'%s' most be a SINGLE token", tc->input
+      );
+}
+
+typedef struct {
+  char input[32];
+  TokenType expected;
+} BoundaryCase;
+
+ParameterizedTestParameters(lexer_kws, keyword_limits) {
+  static const BoundaryCase cases[] = {
+    { "ifx", TOKEN_IDENTIFIER },
+    { "if2", TOKEN_IDENTIFIER },
+    { "if_x", TOKEN_IDENTIFIER },
+    { "dox", TOKEN_IDENTIFIER },
+    { "dou", TOKEN_IDENTIFIER },
+    { "in", TOKEN_IDENTIFIER },
+    { "includex", TOKEN_IDENTIFIER },
+    { "autoX", TOKEN_IDENTIFIER },
+    { "break2", TOKEN_IDENTIFIER },
+  };
+  return cr_make_param_array(BoundaryCase, cases,
+      sizeof(cases) / sizeof(BoundaryCase)
+      );
+}
+ 
+ParameterizedTest(BoundaryCase *tc, lexer_kws, keyword_limits) {
+  Token t = next_token(tc->input);
+  cr_assert_eq(t.type, tc->expected,
+      "Error in '%s': expected %d but got %d", tc->input, tc->expected, t.type
+      );
+  Token e = scan_token();
+  cr_assert_eq(e.type, TOKEN_EOF,
+      "'%s' must be consumed as a SINGLE token", tc->input
+      );
+}
+
+Test(lexer_kws, keyword_in_stream) {
+  init_scan("in include");
+  Token a = scan_token();
+  cr_assert_eq(a.type, TOKEN_IDENTIFIER);
+  Token b = scan_token();
+  cr_assert_eq(b.type, TOKEN_INCLUDE);
+  Token e = scan_token();
+  cr_assert_eq(e.type, TOKEN_EOF);
 }
